b6d7e040ec8d3df427d1dc19ba8ac14140984929
cow <cow-co@users.noreply.github.com>
2022-01-10 19:57:50 +0000

    Initialise Spring Boot Project

---
+++ src/main/java/cowco/tasking/taskingbackend/TaskingBackendApplication.java
@@ -0,0 +1,13 @@
+package cowco.tasking.taskingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskingBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cowco/tasking/taskingbackend/TaskingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cowco.tasking.taskingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
