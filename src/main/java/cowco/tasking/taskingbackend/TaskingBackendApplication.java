412a0c64240bda30b452fc08bf0b71e153eaaf2a
cow <cow-co@users.noreply.github.com>
2022-01-13 17:55:35 +0000

    Disable DataSource Config for now

--- src/main/java/cowco/tasking/taskingbackend/TaskingBackendApplication.java
+++ src/main/java/cowco/tasking/taskingbackend/TaskingBackendApplication.java
@@ -2,8 +2,9 @@ package cowco.tasking.taskingbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class TaskingBackendApplication {
 
 	public static void main(String[] args) {
