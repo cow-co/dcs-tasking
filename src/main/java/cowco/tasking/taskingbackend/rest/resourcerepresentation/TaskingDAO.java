2fa5f56906d7a6e40c8b14bf4be7a4dd734ce1b4
cow <cow-co@users.noreply.github.com>
2022-01-15 15:54:59 +0000

    Return an Empty List for Now

--- src/main/java/cowco/tasking/taskingbackend/rest/resourcerepresentation/TaskingDAO.java
+++ src/main/java/cowco/tasking/taskingbackend/rest/resourcerepresentation/TaskingDAO.java
@@ -8,4 +8,22 @@ public class TaskingDAO {
     private String summary;
     private String location;
     private TaskingType type;
+
+    public TaskingDAO(String summary, String location, TaskingType type) {
+        this.summary = summary;
+        this.location = location;
+        this.type = type;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public TaskingType getType() {
+        return type;
+    }
 }
