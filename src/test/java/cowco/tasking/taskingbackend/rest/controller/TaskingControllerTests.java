38d46e5f03c057c6c2393ed5bbb74e6006068887
cow <cow-co@users.noreply.github.com>
2022-03-03 05:45:22 +0000

    Get Ready to Implement Tests

---
+++ src/test/java/cowco/tasking/taskingbackend/rest/controller/TaskingControllerTests.java
@@ -0,0 +1,24 @@
+package cowco.tasking.taskingbackend.rest.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles(profiles = { "test" })
+@SpringBootTest
+public class TaskingControllerTests {
+    @Test
+    public void testGetsEmptyList() {
+
+    }
+
+    @Test
+    public void testGetsPopulatedList() {
+
+    }
+
+    @Test
+    public void testCreatesSuccessfully() {
+
+    }
+}
