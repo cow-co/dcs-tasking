b38f48e24922e49f34c02bbd3e3bfb4a321a2b3b
cow <cow-co@users.noreply.github.com>
2022-04-14 23:13:15 +0100

    Enable CSRF

--- src/main/java/cowco/tasking/taskingbackend/common/SecurityConfig.java
+++ src/main/java/cowco/tasking/taskingbackend/common/SecurityConfig.java
@@ -4,12 +4,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity security) throws Exception {
-        security.httpBasic().disable().formLogin().disable();
+        security.httpBasic().disable().formLogin().disable()
+                .csrf(csrf -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()));
     }
 }
